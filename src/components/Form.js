e6b322613ad9c47f1c2514d429d7b1a78e0fea11
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2019-12-18 11:42:53 +0100

    Added the Real Add Contact Form fields for the Application

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,106 +1,108 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { Formik } from 'formik';
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-// A custom validation function. This must return an object
-// Which keys are symmetrical to our values / initalValues
-// const validate = values => {
-//   const errors = {};
 
-//   if (!values.firstName) {
-//     errors.firstName = 'Required';
-//   } else if (values.firstName.length > 15) {
-//     errors.firstName = 'Must be 15 characters or less';
-//   }
-
-//   if (!values.lastName) {
-//     errors.lastName = 'Required';
-//   } else if (values.lastName.length > 20) {
-//     errors.lastName = 'Must be 20 characters or less';
-//   }
-
-//   if (!values.email) {
-//     errors.email = 'Required';
-//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-//     errors.email = 'Invalid email address';
-//   }
-
-//   return errors;
-// };
-
-const Form = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will be called when the form is submitted
-  // const formik = useFormik({
-  //   initialValues: {
-  //     firstName: '',
-  //     lastName: '',
-  //     email: 'foo@email.bar',
-  //   },
-
-  //   validationSchema: Yup.object({
-  //     firstName: Yup.string()
-  //       .max(15, 'Must be 15 characters or less')
-  //       .required('Required'),
-
-  //     lastName: Yup.string()
-  //       .max(20, 'Must be 20 characters or less')
-  //       .required('Required'),
-
-  //     email: Yup.string()
-  //       .email('Invalid email address')
-  //       .required('Required'),
-  //   }),
-
-  //   onSubmit: values => {
-  //     console.log(JSON.stringify(values));
-  //   },
-  // });
+const AddContactForm = () => {
 
   return (
     <Formik
-      initialValues={{ firstName: '', lastName: '', email: '' }}
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        middleName: '',
+        nickName: '',
+        phoneNo: '',
+        homeAddress: '',
+        officeAddress: '',
+        facebook: '',
+        twitter: '',
+        linkedIn: '',
+        relationship: '',
+      }}
+
       validationSchema={Yup.object({
         firstName: Yup.string()
-          .max(15, 'Must be 15 characters or less')
-          .required('Required'),
+          .max(15, 'first name must be 15 characters or less')
+          .min(3, 'first name must be 3 characters or more')
+          .required('first name must be provided'),
 
         lastName: Yup.string()
-          .max(20, 'Must be 20 characters or less')
-          .required('Required'),
+          .max(15, 'last name must be 15 characters or less')
+          .min(3, 'last name must be 3 characters or more')
+          .required('last name must be provided'),
 
-        email: Yup.string()
-          .email('Invalid email address')
-          .required('Required'),
+        middleName: Yup.string(),
+        nickName: Yup.string(),
+        phoneNo: Yup.string()
+          .max(12, 'phone number must be 15 characters or less'),
+        homeAddress: Yup.string()
+          .max(20, 'home Address must be 20 characters or less')
+          .min(10, 'home Address must be 10 characters or more')
+          .required(),
+        officeAddress: Yup.string()
+          .max(20, 'office Address must be 20 characters or less')
+          .min(10, 'office Address must be 10 characters or more'),
+        facebook: Yup.string(),
+        twitter: Yup.string(),
+        linkedIn: Yup.string(),
+        relationship: Yup.string()
+          .max(10, 'relationship must be 10 characters or less')
+          .min(5, 'relationship must be 5 characters or more')
+          .required('relationship with contact is required')
       })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          console.log(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 1000);
-      }}
     >
-      {formik => (
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="firstName">First Name</label>
-          <input id="firstName" {...formik.getFieldProps('firstName')} />
-          {formik.touched.firstName && formik.errors.firstName && (
-            <div>{formik.errors.firstName}</div>
-          )}
+      
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" type="text" placeholder="Enter contact first name..." />
+        <ErrorMessage name="firstName" />
 
-          <label htmlFor="lastName">Last Name</label>
-          <input id="lastName" {...formik.getFieldProps('lastName')} />
-          {formik.touched.lastName && formik.errors.lastName && <div>{formik.errors.lastName}</div>}
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" type="text" placeholder="Enter contact last name..." />
+        <ErrorMessage name="lastName" />
 
-          <label htmlFor="email">Email Address</label>
-          <input id="email" {...formik.getFieldProps('email')} />
-          {formik.touched.email && formik.errors.lastName && <div>{formik.errors.email}</div>}
-          <button type="submit">Submit</button>
-        </form>
-      )}
+        <label htmlFor="middleName">Middle name</label>
+        <Field name="middleName" type="text" placeholder="Enter contact middle name..." />
+        <ErrorMessage name="middleName" />
+
+        <label htmlFor="nickName">Nick Name</label>
+        <Field name="nickName" type="text" placeholder="Enter contact nick name..." />
+        <ErrorMessage name="nickName" />
+
+        <label htmlFor="phoneNo">Phone Number</label>
+        <Field name="phoneNo" type="text" placeholder="Enter contact phone number..." />
+        <ErrorMessage name="phoneNo" />
+
+        <label htmlFor="homeAddress">Home Address</label>
+        <Field name="homeAddress" type="text" placeholder="Enter contact home address..." />
+        <ErrorMessage name="homeAddress" />
+
+        <label htmlFor="officeAddress">Office Address</label>
+        <Field name="officeAddress" type="text" placeholder="Enter contact office address..." />
+        <ErrorMessage name="officeAddress" />
+
+        <label htmlFor="facebook">Facebook Link</label>
+        <Field name="facebook" type="text" placeholder="e.g https://facebook.com/johndoe" />
+        <ErrorMessage name="facebook" />
+
+        <label htmlFor="twitter">Twitter Link</label>
+        <Field name="twitter" type="text" placeholder="e.g https://twitter.com/johndoe" />
+        <ErrorMessage name="twitter" />
+
+        <label htmlFor="linkedIn">LinkedIn Link</label>
+        <Field name="linkedIn" type="text" placeholder="e.g https://linkedin.com/johndoe" />
+        <ErrorMessage name="linkedIn" />
+
+        <label htmlFor="relationship">Relationship</label>
+        <Field name="relationship" type="text" placeholder="Enter relationship with contact. e.g Friend" />
+        <ErrorMessage name="relationship" />
+      </Form>
+
     </Formik>
   );
 };
 
-export default Form;
+export default AddContactForm;
